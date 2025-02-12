b554d56778db9e6107f6348f72fce5a19955b3c7
Denny Rubino <rubidenny@gmail.com>
2025-02-12 22:59:07 +0100

    Add searchbar

---
+++ Boolflix/src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <h2>Lista film e serie</h2>
+    </main>
+  );
+}
---
+++ Boolflix/src/components/SearchBar.jsx
@@ -0,0 +1,8 @@
+export default function SearchBar() {
+  return (
+    <form>
+      <input type="search" placeholder="Cerca un film o una serie tv" />
+      <button>Cerca</button>
+    </form>
+  );
+}
